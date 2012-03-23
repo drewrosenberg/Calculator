ad6c04daa8aafc0272465629cda4b1dac1f12efc
Drew Rosenberg <drewrosenberg@Andrews-Mac.local>
2012-03-22 23:26:53 -0700

    first real commit

--- Calculator/CalculatorViewController.h
+++ Calculator/CalculatorViewController.h
@@ -9,5 +9,7 @@
 #import <UIKit/UIKit.h>
 
 @interface CalculatorViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *display;
+@property (weak, nonatomic) IBOutlet UILabel *keylog;
 
 @end
--- Calculator/CalculatorViewController.m
+++ Calculator/CalculatorViewController.m
@@ -7,28 +7,117 @@
 //
 
 #import "CalculatorViewController.h"
+#import "CalculatorBrain.h"
 
-@interface CalculatorViewController ()
 
+@interface CalculatorViewController()
+@property (nonatomic) BOOL userIsInTheMiddleOfEnteringANumber;
+@property (nonatomic) BOOL decimalPressed;
+@property (nonatomic, strong) CalculatorBrain *brain;
 @end
 
 @implementation CalculatorViewController
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+//----- synthesize displays ---------//
+@synthesize display = _display; 
+@synthesize keylog = _keylog;
+
+//------- synthesize properties -----//
+@synthesize userIsInTheMiddleOfEnteringANumber = _userIsInTheMiddleOfEnteringANumber;
+@synthesize decimalPressed = _decimalPressed;
+
+//------- synthesize and init model ----------//
+@synthesize brain = _brain;
+- (CalculatorBrain *)brain{
+    if (!_brain) _brain = [[CalculatorBrain alloc] init];
+    return _brain;
 }
 
-- (void)viewDidUnload
-{
+
+//-------- check to see if a character has been entered--//
+-(BOOL)pressed:(NSString *)character {
+    NSRange textRange;
+    textRange = [self.display.text rangeOfString:character];
+    
+    if (textRange.location != NSNotFound){
+        return YES;
+    }
+    else{return NO;}
+    
+    //------------------------------------
+}
+
+//------- React to Buttons ------------------//
+- (IBAction)digitPressed:(UIButton *)sender{
+
+    //---- Get digit from button title
+    NSString *digit = [sender currentTitle];
+    NSLog(@"digit pressed = %@", digit);
+    
+    if ([self.display.text isEqualToString:@"0"])
+    {
+        //no digit has been entered yet so don't append zeros
+        if ([@"0" isEqualToString:digit]){return;}
+    }
+    
+    if ([@"." isEqualToString:digit]){
+        if (![self pressed:@"."]){
+            self.display.text = [self.display.text stringByAppendingString:digit];
+            self.keylog.text = [self.keylog.text stringByAppendingString:digit];
+        }
+    }
+    else if ([self.display.text isEqualToString:@"0"]){
+        if (![@"0" isEqualToString:digit]){
+            self.display.text = digit;
+            self.keylog.text = [self.keylog.text stringByAppendingString:digit];             
+        }
+    }
+    else {
+        self.display.text = [self.display.text stringByAppendingString:digit];
+        self.keylog.text = [self.keylog.text stringByAppendingString:digit];             
+    }
+}
+
+- (IBAction)operationPressed:(UIButton *)sender {     
+    NSString *operation = [sender currentTitle];
+    
+    if (self.userIsInTheMiddleOfEnteringANumber){[self enterPressed];}
+    else{
+        self.keylog.text =[self.keylog.text stringByAppendingString:@" "];
+        }
+    
+    //log the operation and space to the key log
+    self.keylog.text = [self.keylog.text stringByAppendingString:operation];
+    self.keylog.text = [self.keylog.text stringByAppendingString:@" "];
+    
+    double result = [self.brain performOperation:operation];
+    NSString *resultString = [NSString stringWithFormat:@"%g", result];
+  
+    [self.brain pushOperand:result];
+    self.display.text = resultString;
+}
+- (IBAction)enterPressed {
+    //add a space to the log
+    self.keylog.text = [self.keylog.text stringByAppendingString:@" "];
+
+    [self.brain pushOperand:[self.display.text doubleValue]];
+    self.userIsInTheMiddleOfEnteringANumber = NO;
+    self.decimalPressed = NO;
+
+    
+}
+- (IBAction)clearPressed {
+    self.userIsInTheMiddleOfEnteringANumber = NO;
+    self.decimalPressed = NO;
+    self.display.text = @"0";
+    self.keylog.text = @"";
+    [self.brain clearOperands];
+}
+//---------------------------------------------
+
+- (void)viewDidUnload {
+    [self setDisplay:nil];
+    [self setKeylog:nil];
     [super viewDidUnload];
-    // Release any retained subviews of the main view.
 }
-
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
-{
-    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
-}
-
 @end
