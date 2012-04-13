24433b20f22babc8fa2f552499559abeffe483cb
Drew Rosenberg <drewrosenberg@gmail.com>
2012-04-12 21:59:47 -0700

    Added graphviewcontroller

---
+++ Calculator/GraphViewController.h
@@ -0,0 +1,13 @@
+//
+//  GraphViewController.h
+//  Calculator
+//
+//  Created by Drew Rosenberg on 4/12/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GraphViewController : UIViewController
+
+@end
