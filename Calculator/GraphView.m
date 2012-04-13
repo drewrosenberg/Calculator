3cdf8fc73b90a94cb47b16a0add327b4364bc61b
Drew Rosenberg <drewrosenberg@gmail.com>
2012-04-13 09:21:10 -0700

    Added axes to drawrect on graphview

---
+++ Calculator/GraphView.m
@@ -0,0 +1,35 @@
+//
+//  GraphView.m
+//  Calculator
+//
+//  Created by Drew Rosenberg on 4/12/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "GraphView.h"
+#import "AxesDrawer.h"
+
+@implementation GraphView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code         
+    }
+    return self;
+}
+
+
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    CGPoint origin;
+    origin.x = rect.size.width / 2;
+    origin.y = rect.size.height / 2;
+    [AxesDrawer drawAxesInRect:rect originAtPoint:origin scale:10];
+}
+
+
+@end
