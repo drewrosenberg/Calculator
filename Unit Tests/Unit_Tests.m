9d0644772a7a6a1cc991b36579cda3efb6725b44
Drew Rosenberg <drewrosenberg@gmail.com>
2012-06-30 22:36:29 -0700

    reverting back

---
+++ Unit Tests/Unit_Tests.m
@@ -0,0 +1,32 @@
+//
+//  Unit_Tests.m
+//  Unit Tests
+//
+//  Created by Drew Rosenberg on 6/30/12.
+//
+//
+
+#import "Unit_Tests.h"
+
+@implementation Unit_Tests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Unit Tests");
+}
+
+@end
